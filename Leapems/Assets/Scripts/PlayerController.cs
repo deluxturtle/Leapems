8f10d3739ef4201a75e08ef4af0d1847a99fa4f5
deluxturtle <sebaturtle@gmail.com>
2018-08-14 13:09:51 -0500

    got a cool crosshairmade

--- Leapems/Assets/Scripts/PlayerController.cs
+++ Leapems/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Author: Andrew Seba
@@ -8,19 +9,27 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour {
 
+    public float aimMultiplier = 1f;
+    public GameObject crosshair;
+    //Slider that shows when my action (or gun or jump "TBD") will happen.
+    public Slider popSlider;
+    //Limit on the slider value. (Trigger value)
+    public float actionSliderLimit = 1f;
+
     Vector2 startPos;
     Vector2 endPos;
     bool mouseDown = false;
 
 	// Use this for initialization
 	void Start () {
-		
+        popSlider.maxValue = actionSliderLimit;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
+            crosshair.SetActive(true);
             startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mouseDown = true;
             StartCoroutine("MouseDownLoop");
@@ -28,6 +37,7 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetMouseButtonUp(0))
         {
+            crosshair.SetActive(false);
             mouseDown = false;
         }
 
@@ -40,9 +50,13 @@ public class PlayerController : MonoBehaviour {
         {
             endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 distance = endPos - startPos;
-            Debug.Log(distance.magnitude);
+            Vector2 target = (new Vector2(-distance.x, -distance.y) * aimMultiplier)  + (Vector2)transform.position;
+            crosshair.transform.position = target;
+            popSlider.value = distance.magnitude;
             if(distance.magnitude >= 1)
             {
+                Debug.Log("Fire!");
+                crosshair.SetActive(false);
                 mouseDown = false;
             }
             yield return null;
