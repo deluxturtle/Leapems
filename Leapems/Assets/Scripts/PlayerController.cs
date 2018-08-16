05d372ba8d7b0333eef862cf96f614b37c975ebc
deluxturtle <sebaturtle@gmail.com>
2018-08-16 09:00:15 -0500

    Got a speed out of the mouse.
    Also found out what I was doing wrong with all my pixel visual experiments. I needed to snap it to the grid not just visually make it snap to a grid.

--- Leapems/Assets/Scripts/PlayerController.cs
+++ Leapems/Assets/Scripts/PlayerController.cs
@@ -19,9 +19,10 @@ public class PlayerController : MonoBehaviour {
     Vector2 startPos;
     Vector2 endPos;
     bool mouseDown = false;
+    float[] lastThree = new float[3];
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         popSlider.maxValue = actionSliderLimit;
 	}
 	
@@ -46,20 +47,65 @@ public class PlayerController : MonoBehaviour {
 
     IEnumerator MouseDownLoop()
     {
+        float prevMag = -1;
+        float curMag;
+        float diff;
         while (mouseDown)
         {
             endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 distance = endPos - startPos;
             Vector2 target = (new Vector2(-distance.x, -distance.y) * aimMultiplier)  + (Vector2)transform.position;
+            float modx = target.x % 0.032f;
+            float mody = target.y % 0.032f;
+            target = new Vector2(target.x - modx, target.y - mody);
             crosshair.transform.position = target;
             popSlider.value = distance.magnitude;
+
+            //If prevmag hasn't been assigned yet. start the process
+            if(prevMag < 0)
+            {
+                prevMag = distance.magnitude;
+            }
+            else//Then get the speed of the mouse.
+            {
+                curMag = distance.magnitude;
+                diff = curMag - prevMag;
+                if(diff > 0)
+                {
+                    AddToLastThree(diff);
+                }
+                prevMag = curMag;
+            }
+            
             if(distance.magnitude >= 1)
             {
-                Debug.Log("Fire!");
+                Debug.Log("Fire! @ speeds of " + GetLargestDelta());
                 crosshair.SetActive(false);
                 mouseDown = false;
             }
             yield return null;
         }
+    }
+
+    void AddToLastThree(float pDelta)
+    {
+        //for(int i = 0; i < lastThree.Length - 1; i++)
+        //{
+        //    lastThree[i] = lastThree[i + 1];
+        //}
+        lastThree[0] = lastThree[1];
+        lastThree[1] = lastThree[2];
+        lastThree[2] = pDelta;
+    }
+
+    float GetLargestDelta()
+    {
+        float biggestNum = lastThree[0];
+        for(int i = 1; i < lastThree.Length; i++)
+        {
+            if (lastThree[i] > biggestNum)
+                biggestNum = lastThree[i];
+        }
+        return biggestNum;
     }
 }
